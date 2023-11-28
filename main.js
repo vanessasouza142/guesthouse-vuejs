ea9dcb745028aea0ab94ca11f9f8001b801bc162
vanessasouza142 <vanessa.kbs16@gmail.com>
2023-11-27 22:28:43 -0300

    Detalhes da pousada/quartos e consulta de disponibilidade de um quarto

--- main.js
+++ main.js
@@ -1,45 +1,41 @@
-
 const app = Vue.createApp({
-  data(){
-    return{
+  data() {
+    return {
       searchText: '',
       listGuesthouses: [],
-      selectedGuesthouse: null
-    }
+      selectedGuesthouse: null,
+      listRooms: [],
+      totalPrice: null,
+      availabilityMessage: null,
+    };
   },
 
-  computed:{
-    listResult(){
-
-      if(this.searchText){
+  computed: {
+    listResult() {
+      if (this.searchText) {
         return this.listGuesthouses.filter(guesthouse => {
           return guesthouse.brand_name.toLowerCase().includes(this.searchText.toLowerCase());
         });
-      }else {
+      } else {
         return this.listGuesthouses;
       }
     }
   },
 
-  methods:{
+  methods: {
     async getData() {
       let response = await fetch('http://localhost:3000/api/v1/guesthouses');
       let data = await response.json();
-      this.listGuesthouses = [];
-
-      data.forEach(item => {
-        var guesthouse = new Object();
-        guesthouse.id = item.id;
-        guesthouse.brand_name = item.brand_name;
-        guesthouse.city = item.city;
-
-        this.listGuesthouses.push(guesthouse);
-      });
+      this.listGuesthouses = data.map(item => ({
+        id: item.id,
+        brand_name: item.brand_name,
+        city: item.city
+      }));
     },
+
     async getDetails(guesthouseId) {
       let response = await fetch(`http://localhost:3000/api/v1/guesthouses/${guesthouseId}`);
       let data = await response.json();
-      console.log(data);
       this.selectedGuesthouse = {
         id: data.id,
         average_score: data.average_score,
@@ -57,18 +53,47 @@ const app = Vue.createApp({
         check_out: data.check_out
       };
     },
+
     async getRooms(guesthouseId) {
       let response = await fetch(`http://localhost:3000/api/v1/guesthouses/${guesthouseId}/rooms`);
       let data = await response.json();
-      console.log(data);
-      // this.selectedGuesthouse = {
-      //   id: data.id,
-      //   name: data.name,
-      //   description: data.description,
-      //   area: data.area,
-      //   max_guests: data.max_guests,
-      //   default_price: data.default_price,
-       // };
+      this.listRooms = data.map(item => ({
+        guesthouseId: guesthouseId,
+        id: item.id,
+        name: item.name,
+        description: item.description,
+        area: item.area,
+        max_guest: item.max_guest,
+        default_price: item.default_price,
+        totalPrice: null,
+        availabilityMessage: null
+      }));
+    },
+
+    async checkAvailability(room) {
+      const requestBody = {
+        check_in_date: room.checkInDate,
+        check_out_date: room.checkOutDate,
+        guests_number: room.guestsNumber,
+        room: room.id
+      };
+    
+      const response = await fetch(`http://localhost:3000/api/v1/rooms/${room.id}/bookings/check_availability`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ booking: requestBody })
+      });
+    
+      if (response.ok) {
+        const data = await response.json();
+        room.totalPrice = data.total_price;
+        room.availabilityMessage = null;
+      } else {
+        room.totalPrice = null;
+        room.availabilityMessage = 'O quarto não está disponível para o período selecionado.';
+      }
     }
   }
 });
